7563a30cb244d8068cff189681e958e22b4794f5
devpalqar-hub <dev.palqar@gmail.com>
2026-08-24 21:32:58 +0530

    build fix

--- src/pages/Profile/AddressModal.tsx
+++ src/pages/Profile/AddressModal.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { X, MapPin } from "lucide-react";
+import { X } from "lucide-react";
 import styles from "./AddressModal.module.css";
 import type { AddressPayload } from "../../types/address";
 import { createAddress } from "../../services/addressService";
